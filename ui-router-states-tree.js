fa1c2fe4515287ff3ca0bdeb2094c02d02f87b7f
Nick Serebrennikov <non.manifold@gmail.com>
2015-01-15 23:06:34 +0300

    feat: States have now property 'children'

---
+++ ui-router-states-tree.js
@@ -0,0 +1,64 @@
+'use strict';
+
+(function () {
+    /**
+     * @ngdoc overview
+     * @name statesTree
+     */
+    angular.module('statesTree', ['ui.router']).
+    /**
+     * @ngdoc object
+     * @name statesTree.statesTree
+     * @requires $state
+     */
+        service('statesTree', [
+            '$state',
+
+            function ($state) {
+                var statesTree = {
+                    get: function () {
+                        var tree = [];
+                        angular.forEach($state.get(), function (state) {
+                            placeState(tree, state);
+                        });
+                        return tree;
+                    }
+                };
+
+                return statesTree;
+            }
+        ]);
+
+    function placeState(tree, state) {
+        var path = state.name.split('.');
+        var node = tree;
+        var currPath = [];
+        angular.forEach(path, function (dir) {
+            currPath.push(dir);
+            var isLast = currPath.length === path.length;
+            if (isLast) {
+                //add state at current level:
+                node.push(state);
+            } else {
+                //next path part lays deeper
+                var currPathStr = currPath.join('.');
+                var parent;
+                angular.forEach(node, function (item) {
+                    if(item.name === currPathStr){
+                        parent=item;
+                    }
+                });
+                if (parent) {
+                    if(!parent.hasOwnProperty('children')){
+                        parent.children=[];
+                    }
+                    node = parent.children;
+                } else {
+                    throw 'no parent for state ' + parent + ' ' + state.name;
+                }
+            }
+
+        });
+    };
+
+})();
